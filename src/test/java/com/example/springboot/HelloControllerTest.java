adfee30dac588b44325ab1d7fbacdf287908abd4
timwang <timwang@yunw1G5QVL.vmware.com>
2022-12-20 23:16:11 +0800

    demo

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -26,6 +26,6 @@ class HelloControllerTest {
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu in Taiwan!!"));
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu in Taiwan!!!!!!!!!!!!!"));
     }
 }
