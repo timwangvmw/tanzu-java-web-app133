09a047826a62b2292b9efc9bc3ee48a665a30705
timwangvmw <yunw1@vmware.com>
2023-05-23 13:54:14 +0800

    update source

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -7,8 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
-import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 
 @SpringBootApplication
 public class Application {
@@ -32,8 +32,8 @@ public class Application {
 		};
 	}
 	
-    	@Bean
-	public HttpTraceRepository htttpTraceRepository() {
-		return new InMemoryHttpTraceRepository();
+	@Bean
+	public HttpExchangeRepository htttpTraceRepository() {
+		return new InMemoryHttpExchangeRepository();
 	}
 }
