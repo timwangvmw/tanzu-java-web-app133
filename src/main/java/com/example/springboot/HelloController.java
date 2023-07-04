1d85e7eeb99aeb866ff10d62f5bb983dbbd72fc4
timwangvmw <yunw1@vmware.com>
2023-07-04 18:15:23 +0800

    財經

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + 新光人壽!";
+		return "Greetings from Spring Boot + 財經公司!";
 	}
 
 }
